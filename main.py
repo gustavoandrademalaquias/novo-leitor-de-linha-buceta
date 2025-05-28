bf5aa2977a74d58c6db04819360cd73d8b1063f4
gustavoandrademalaquias <00001107655626sp@al.educacao.sp.gov.br>
2025-05-27 21:40:57 -0300

    Update main.py
    Melhora na estrutura lógica do código.

--- main.py
+++ main.py
@@ -1,23 +1,3 @@
-# 0: preto
-# 1: cor
-
-'''
-
-motorA = robotbit.Motors.M1A
-motorB = robotbit.Motors.M1B
-
-def on_forever():
-    robotbit.motor_run(motorA, 150)
-    robotbit.motor_run(motorB, 150)
-    basic.pause(2500)
-    robotbit.motor_run(motorA, 0)
-    robotbit.motor_run(motorB, 0)
-    basic.pause(2500)
-basic.forever(on_forever)
-
-
-'''
-
 motor_esquerda = robotbit.Motors.M1A
 motor_direita = robotbit.Motors.M1B
 max_speed = 200
@@ -32,52 +12,23 @@ def verificar():
 
     return leitor_esquerda, leitor_direita
 
-
-
 def on_forever():
+    # verificar() == 1 significa que detectou a linha preta (ausência de luz), caso contrário, 0 significa qualquer cor.
     esquerda, direita = verificar()
 
     if esquerda == 1:
-        #detectou linha na esquerda
-        #parar motor direito pra esteira ir p esquerda
-        robotbit.motor_run(
-            motor_direita,
-            max_speed
-        )
-        robotbit.motor_run(
-            motor_esquerda,
-            0
-        )
-        
-    if direita == 1:
-        robotbit.motor_run(
-            motor_esquerda,
-            max_speed
-        )
-        robotbit.motor_run(
-            motor_direita,
-            0
-        )
-        
-    if esquerda == 0 and direita == 0:
-        robotbit.motor_run(
-            motor_direita,
-            normal_speed
-        )
-        robotbit.motor_run(
-            motor_esquerda,
-            normal_speed
-        )
-'''
-    if esquerda == 1 and direita == 1:
-        robotbit.motor_run(
-            motor_direita,
-            -normal_speed
-        )
-        robotbit.motor_run(
-            motor_esquerda,
-            -normal_speed
-        )
- '''       
+        # sensor esquerdo passou na linha
+        robotbit.motor_run(motor_direita, max_speed)
+        robotbit.motor_run(motor_esquerda, 0)
+    elif direita == 1:
+        #sensor direito passou na linha
+        robotbit.motor_run(motor_esquerda, max_speed)
+        robotbit.motor_run(motor_direita, 0)
+    else:
+        #sensores ok
+        robotbit.motor_run(motor_direita, normal_speed)
+        robotbit.motor_run(motor_esquerda, normal_speed)
+
+    basic.pause(100)
 
 basic.forever(on_forever)
