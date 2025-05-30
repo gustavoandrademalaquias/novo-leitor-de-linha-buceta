e0e6dcd2a5640d0d75ae3050cbc560ace59f4d99
gustavoandrademalaquias <00001107655626sp@al.educacao.sp.gov.br>
2025-05-30 15:41:06 -0300

    Update main.py
    new changes (now it follows line but doesn't make curves)

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 motor_esquerda = robotbit.Motors.M1A
 motor_direita = robotbit.Motors.M1B
-max_speed = 200
-normal_speed = 150
+max_speed = 150
+normal_speed = 100
 
 def verificar():
     leitor_esquerda = pins.digital_read_pin(DigitalPin.P1)
     leitor_direita = pins.digital_read_pin(DigitalPin.P2)
 
-    basic.clear_screen()
-    basic.show_string(str(leitor_esquerda) + str(leitor_direita))
+    #basic.clear_screen()
+    #basic.show_string(str(leitor_esquerda) + str(leitor_direita))
 
     return leitor_esquerda, leitor_direita
 
@@ -16,15 +16,15 @@ def on_forever():
     # verificar() == 1 significa que detectou a linha preta (ausência de luz), caso contrário, 0 significa qualquer cor.
     esquerda, direita = verificar()
 
-    if esquerda == 1:
+    if esquerda == 1 and direita == 0:
         # sensor esquerdo passou na linha
         robotbit.motor_run(motor_direita, max_speed)
         robotbit.motor_run(motor_esquerda, 0)
-    elif direita == 1:
+    if esquerda == 0 and direita == 1:
         #sensor direito passou na linha
         robotbit.motor_run(motor_esquerda, max_speed)
         robotbit.motor_run(motor_direita, 0)
-    else:
+    if esquerda == 0 and direita == 0:
         #sensores ok
         robotbit.motor_run(motor_direita, normal_speed)
         robotbit.motor_run(motor_esquerda, normal_speed)
